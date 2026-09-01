4d8fbc5a8c6826d83b4350350fb813d954dab702
James Davidson <jamesd3142@gmail.com>
2026-09-01 14:21:07 +0800

    Set up site generator

---
+++ csv.md
@@ -0,0 +1,9 @@
+# CSV
+
+The widely used Comma Separated Values. A format loved and loathed.
+
+GitHub: https://github.com/dmiller/data.csv
+
+NuGet: n/a
+
+deps: `io.github.dmiller/data.csv {:git/sha "faa419961f3a74aa113c7c7ef7ec6f1bc6d9458e"}`
---
+++ index.clj
@@ -0,0 +1,23 @@
+(ns nextjournal.blog.index
+  {:nextjournal.clerk/visibility {:code :hide}}
+  (:require [nextjournal.clerk :as clerk]
+            [nextjournal.clerk.parser :as parser]))
+
+(clerk/html [:style "h1 { margin-bottom: 0; font-size: 2.6em !important;}
+h1 + p { font-size: 1.7em; position: relative; padding-bottom: 40px;}
+h1 + p:after { content: \"\"; position: absolute; left: 0; bottom: 0; border-bottom: 2px solid #ccc; width: 350px;"])
+
+;; # awesome-clojure-clr
+;; Discover the ClojureCLR ecosystem
+
+{:nextjournal.clerk/visibility {:result :hide}}
+
+(defn parse [post-id]
+  (->> (str post-id ".md") (parser/parse-file {:doc? true})))
+
+(defn link-post [post-id]
+  (let [{:keys [title file]} (parse post-id)]
+    [:h2 [:a {:href (clerk/doc-url file)} title]]))
+
+^{::clerk/viewer clerk/html :nextjournal.clerk/visibility {:result :show}}
+(into [:<>] (map link-post) '[csv])
